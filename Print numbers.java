30a59d1752f6b03500e006c1fa2c2ec6a87ce4d5
Anusha Joshi <anushajoshi2651@gmail.com>
2019-05-14 01:33:42 +0530

    Add files via upload

---
+++ Print numbers.java
@@ -0,0 +1,21 @@
+/*Given the code to print number from 1 to n in increasing order recursively.
+ But it contains few bugs, that you need to rectify such that all the test
+ cases should pass.*/
+
+import java.util.Scanner;
+public class Solution {
+
+	public static int count(int n){
+if( n==1){
+  return 1;
+  
+}
+      int ans=count/10;
+      return ans+1;
+	}
+    public static void main(String[]args){
+  Scanner s=new Scanner(System.in);
+	int x=s.nextInt();
+	int num=x;
+	print (num+);
+}
