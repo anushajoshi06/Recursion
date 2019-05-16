7d3614e5c5fb111df5da488f08ccdf6c010a8075
Anusha Joshi <anushajoshi2651@gmail.com>
2019-05-17 00:12:19 +0530

    Add files via upload

---
+++ First index of number.java
@@ -0,0 +1,37 @@
+/*First Index of Number
+
+Given an array of length N and an integer x, you need to find and return the first index of integer x present in the array. Return -1 if it is not present in the array.
+First index means, the index of first occurrence of x in the input array.
+Do this recursively. Indexing in the array starts from 0.
+Input Format :
+Line 1 : An Integer N i.e. size of array
+Line 2 : N integers which are elements of the array, separated by spaces
+Line 3 : Integer x
+Output Format : first index or -1
+Constraints : 1 <= N <= 10^3
+Sample Input :
+4
+9 8 10 8
+8
+Sample Output :
+1
+*/
+
+
+public class Solution {
+
+	public static int firstIndex(int input[], int x) {
+		
+	  int n=input.length-1;
+      if(x==input[0]){
+        return 0;
+      }
+      int[] a = new int[n];
+      for(int i = 0; i< a.length; i++)
+      {
+      	a[i] = input[i+1];
+      }
+      int ans = firstIndex(a,x);
+      return ans;
+    }
+}
